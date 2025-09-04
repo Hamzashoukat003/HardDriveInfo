cb653c98bcdb1a5c8ef0b6c81c85a15dde54dcb8
Hamzashokat003 <hamzashoukat32@outlook.com>
2025-09-04 14:02:01 +0500

    Add Code Files

---
+++ HardDriveInfo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace HardDriveInfo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(50, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.Text = "Get Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labels...
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 60);
+            this.label1.Text = "Drive Name: ";
+
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 80);
+
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 100);
+
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 120);
+
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 140);
+
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 160);
+
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 180);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Text = "Drive Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ HardDriveInfo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HardDriveInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(Environment.GetLogicalDrives());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) return;
+
+            DriveInfo drive = new DriveInfo(comboBox1.SelectedItem.ToString());
+
+            label1.Text = "Drive Name: " + drive.Name;
+            label2.Text = "Volume Label: " + drive.VolumeLabel;
+            label3.Text = "Drive Type: " + drive.DriveType;
+            label4.Text = "Drive Format: " + drive.DriveFormat;
+            label5.Text = "Total Size: " + (drive.TotalSize / (1024 * 1024 * 1024)) + " GB";
+            label6.Text = "Free Disk Space: " + (drive.TotalFreeSpace / (1024 * 1024 * 1024)) + " GB";
+            label7.Text = "Drive Ready: " + drive.IsReady;
+        }
+    }
+}
